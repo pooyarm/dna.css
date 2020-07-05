25eb3492056e7eef725eedd12610d125789331b2
Pooya Rostam <pooya.rostam@eventinc.de>
2020-07-05 20:16:50 +0200

    feat: basic font color genes

--- catalog/pages/color/index.jsx
+++ catalog/pages/color/index.jsx
@@ -2,20 +2,34 @@ import React from 'react'
 
 const Color = () => {
   return (
-    <>
-      {['primary', 'secondary', 'red', 'green', 'foreground', 'background'].map((color, index) => (
-        <React.Fragment key={index}>
-          {new Array(['primary', 'secondary'].includes(color)? 9 : 5).fill(0).map((_value, colorIndex) => {
-            const className = `bg--${color}--${colorIndex}`
-            return (
-              <div className={className} key={colorIndex}>
-                {`.${className}`}
-              </div>
-            )
-          })}
-        </React.Fragment>
-      ))}
-    </>
+    <table>
+      <thead>
+        <tr>
+          <th>background</th>
+          <th>font color</th>
+        </tr>
+      </thead>
+      <tbody>
+        {['primary', 'secondary', 'red', 'green', 'foreground', 'background'].map((color, index) => (
+          <React.Fragment key={index}>
+            {new Array(['primary', 'secondary'].includes(color)? 9 : 5).fill(0).map((_value, colorIndex) => {
+              const bg = `bg--${color}--${colorIndex}`
+              const fc = `fc--${color}--${colorIndex}`
+              return (
+                <tr key={colorIndex}>
+                  <td className={bg}>
+                    {`.${bg}`}
+                  </td>
+                  <td className={fc}>
+                    {`.${fc}`}
+                  </td>
+                </tr>
+              )
+            })}
+          </React.Fragment>
+        ))}
+      </tbody>
+    </table>
   )
 }
 
